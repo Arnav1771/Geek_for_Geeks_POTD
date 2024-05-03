af7d7d87cd3b31398d271288c15918040df21f09
Stealth <60522445+Arnav1771@users.noreply.github.com>
2024-05-04 01:10:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.py
@@ -0,0 +1,77 @@
+#User function Template for python3
+
+'''
+class Node:
+            def __init__(self, data):
+                self.data = data
+                self.left = self.right = None
+'''
+
+#Function to return a tree created from postorder and inoreder traversals.
+class Solution:
+    def buildTree(self, inorder, postorder, n):
+        if n == 0:
+            return None
+        
+        root_val = postorder[-1]
+        root = Node(root_val)
+        
+        root_index = inorder.index(root_val)
+        
+        root.right = self.buildTree(inorder[root_index + 1:], postorder[root_index:n-1], n - root_index - 1)
+        root.left = self.buildTree(inorder[:root_index], postorder[:root_index], root_index)
+        
+        return root
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+from collections import  defaultdict
+
+#Contributed by : PranchalK
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+
+# Helper function that allocates  
+# a new node  
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+# This funtcion is here just to test  
+def preOrder(node):
+    if node == None:
+        return
+    print(node.data, end=" ")
+    preOrder(node.left)
+    preOrder(node.right)
+    
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())  # number of nodes in tree
+        in_order = list(map(int, input().strip().split()))  # parent child info in list
+        post_order = list(map(int, input().strip().split()))  # parent child info in list
+        ob = Solution()
+        preOrder(ob.buildTree(in_order,post_order,n))
+        print()
+
+
+# } Driver Code Ends
